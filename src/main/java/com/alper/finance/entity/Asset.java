6e92f402ec5f8729b132e74dc700f7c19be5fc5b
AlperOzdamar <alper.ozdamar@gmail.com>
2021-02-15 00:09:31 -0500

    Unused imports removed

--- src/main/java/com/alper/finance/entity/Asset.java
+++ src/main/java/com/alper/finance/entity/Asset.java
@@ -1,15 +1,6 @@
 package com.alper.finance.entity;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import org.springframework.format.annotation.DateTimeFormat;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Transient;
+import javax.persistence.*;
 import java.util.Date;
 
 @Entity
