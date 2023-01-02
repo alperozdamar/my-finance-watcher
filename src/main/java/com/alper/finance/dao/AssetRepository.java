4d3fdd9753999f7bb8b94b314e298f1f08bc56a7
alperozdamar <alper.ozdamar@gmail.com>
2023-01-01 20:09:13 -0500

    Order Date in ASC

--- src/main/java/com/alper/finance/dao/AssetRepository.java
+++ src/main/java/com/alper/finance/dao/AssetRepository.java
@@ -15,7 +15,7 @@ public interface AssetRepository extends JpaRepository<Asset, Integer> {
     //  @Query("FROM Asset a where a.month = :month")
     //  public List<Asset> findByMonth(@Param("month") int month);
 
-    @Query("SELECT a FROM Asset a order by a.date")
+    @Query("SELECT a FROM Asset a order by a.date ASC")
     public List<Asset> findAll();
 
     public List<Asset> findById(int id);
